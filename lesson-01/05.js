c929908cc7f7dd8467d975f7b3b9017bfc0399e2
yahve0110 <ilik124darc@gmail.com>
2023-12-05 20:10:22 +0200

    lesson 4

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,15 +1,13 @@
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
-switch (true) {
+switch(true){
     case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
-        break;
-    case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
-        break;
-    case rule.length === 25:
-        console.log("50 на 50");
+        console.log("Все таки нет правил без исключения"); 
+        break; 
+     case rule.length > 25:
+         console.log("У меня 100 пудов все получится"); 
         break;
     default:
-        console.log("Что-то пошло не так");
+        console.log("У меня 100 пудов все получится"); 
+        break;
 }
