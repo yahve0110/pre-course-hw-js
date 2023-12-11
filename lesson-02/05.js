82939bf60bc4c81de10ff56a6aaeabbba97cdca8
yahve0110 <ilik124darc@gmail.com>
2023-12-11 09:25:00 +0200

    lesson2

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -8,5 +8,5 @@ let passportCopy = Object.assign({}, passport)
 
 passportCopy.name = "Ivan"
 
-console.log(passport);
-console.log(passportCopy);
+console.log(passport.name);
+console.log(passportCopy.name);
