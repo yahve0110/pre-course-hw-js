8d379eb78452e7561010786327046de6ca92626a
yahve0110 <ilik124darc@gmail.com>
2023-12-19 11:46:20 +0200

    week 3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter((word)=>word === "черный" || word === "красный" || word === "желтый").join('-')
+}
+
+console.log(createColorString());
